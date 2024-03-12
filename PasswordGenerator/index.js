034b4ee3c9b4f865a50538de73c5a1c162dce032
Mr.2608 <shubhamsoni2699@gmail.com>
2024-03-12 10:49:04 +0530

    copy password functionality added

--- PasswordGenerator/index.js
+++ PasswordGenerator/index.js
@@ -5,9 +5,15 @@ let showPassword = document.getElementById("password");
 
 document.getElementById("lengthInput").addEventListener("change",()=>{
     passwordLength = document.getElementById("lengthInput").value;
-})
+});
 
 document.getElementById("buttonPassword").addEventListener("click" , ()=>{
     generatePassword(passwordLength)
     .then( password=>showPassword.value = password);
-})
+});
+
+document.getElementById("buttonCopy").addEventListener("click", ()=>{
+    let password = showPassword.value;
+    navigator.clipboard.writeText(password);
+    alert("Text Copied :-" + password);
+});
