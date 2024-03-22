9792afb29a11d2f2a1d6319da58314887aa0c645
Shubham Soni <shubhamsoni2699@gmail.com>
2024-03-22 15:08:04 +0530

    vite config change

--- CurrencyConverter/vite.config.js
+++ CurrencyConverter/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/CurrencyConverter",
+  base:"https://shubhamsoni2699.github.io/webdev/CurrencyConverter/",
   plugins: [react()],
 })
