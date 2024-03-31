de182b47d5904260e65ff13b9ad03e1a7869cdcc
Shubham Soni <shubhamsoni2699@gmail.com>
2024-03-31 13:33:11 +0530

    suduko algo

---
+++ SudokuSolver/sudokuAlgo.js
@@ -0,0 +1,44 @@
+function isRightPlace(row,col,val,board){
+    for(let i = 0 ; i<9 ; i++){
+        if(board[row][i]==val){
+            return false;
+        }
+        if(board[i][col]==val){
+            return false;
+        }
+        if( board[(3*(Math.floor(row/3))+Math.floor(i/3))][(3*(Math.floor(col/3))+Math.floor(i%3))] ==val ){
+            return false;
+        }
+    }
+    return true;
+}
+
+function solve(board){
+    for(let row = 0 ; row<9 ; row++){
+        for(let col = 0 ; col<9 ; col++){
+            if(board[row][col]==0){
+                for(let val = 1 ; val<=9 ; val++){
+                    if(isRightPlace(row,col,val,board)){
+                        board[row][col] = val;
+                        let isPossible = solve(board);
+                        if(isPossible){
+                            return true;
+                        }else{
+                            board[row][col] = 0;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+async function sudokuSolver(board){
+    console.log(board);
+    await solve(board);
+    console.log(board);
+}
+
+export default sudokuSolver;
