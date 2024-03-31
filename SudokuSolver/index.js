87dd0e16c206ad8a84267d25bc0f53110e2999e5
Shubham Soni <shubhamsoni2699@gmail.com>
2024-03-31 13:59:04 +0530

    set value in input box

--- SudokuSolver/index.js
+++ SudokuSolver/index.js
@@ -3,8 +3,18 @@ let board = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,
 let example = [[3,0,6,5,0,8,4,0,0],[5,2,0,0,0,0,0,0,0],[0,8,7,0,0,0,0,3,1],[0,0,3,0,1,0,0,8,0],[9,0,0,8,6,3,0,0,5],[0,5,0,0,9,0,6,0,0],[1,3,0,0,0,0,2,5,0],[0,0,0,0,0,0,0,7,4],[0,0,5,2,0,6,3,0,0]]
 $(".box").change( function(e){
     board[Number(e.target.id[0])][Number(e.target.id[1])] = Number(e.target.value);
-} )
+} );
+
 
 $(".generate").click( async function (){
     let solution =  await sudokuSolver(board);
-} )
+    setValue(solution)
+} );
+
+async function setValue(boardSol){
+    for(let row = 0 ; row<9 ; row++){
+        for(let col = 0 ; col<9 ; col++){
+            document.getElementById(String(row)+String(col)).value = boardSol[row][col];
+        }
+    }
+}
