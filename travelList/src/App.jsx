c0cd00d32e1a673d148a21b8a7e4f2e56f9798cb
Shubham Soni <shubhamsoni2699@gmail.com>
2024-12-20 16:30:26 +0530

    sorting filter added

--- travelList/src/App.jsx
+++ travelList/src/App.jsx
@@ -79,10 +79,21 @@ function Form({ onAddItems }) {
 }
 
 function TravelList({ items, onDeleteItems, onUpdatePacked }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems = [];
+  if (sortBy === "input") {
+    sortedItems = items;
+  } else if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  } else {
+    sortedItems = items.filter((item) => item.packed);
+  }
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -91,6 +102,13 @@ function TravelList({ items, onDeleteItems, onUpdatePacked }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed</option>
+        </select>
+      </div>
     </div>
   );
 }
