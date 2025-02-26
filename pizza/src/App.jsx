9d471c50b20aeffbec12e1da82eaa9f729661cec
Shubham Soni <shubhamsoni2699@gmail.com>
2025-02-26 22:07:15 +0530

    basic routing betweeen pages

--- pizza/src/App.jsx
+++ pizza/src/App.jsx
@@ -1,6 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./feature/menu/Menu";
+import Cart from "./feature/cart/Cart";
+import Order from "./feature/order/Order";
+import CreateOrder from "./feature/order/CreateOrder";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 function App() {
-  const x = 43;
-  return <div>Hello world ${x}</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
